0186fcfb5e1579c6856c75cf1053c59103d32851
Miguel Fermin <miguel.fermin@whitecloak.com>
2018-03-18 06:07:14 +0800

    Fix deploy order

--- core/contracts/Order.sol
+++ core/contracts/Order.sol
@@ -11,20 +11,17 @@ contract Order {
   uint private status;
   uint private amount;
 
-  /*
-     function Order(address _buyer, address _seller, string _name, string _serialNumber, uint256 _timestamp,
-     uint _warrantyMilliseconds1, uint _warrantyMilliseconds2, uint _status, uint _amount) public {
-     buyer = _buyer;
-     seller = _seller;
-     name = _name;
-     serialNumber = _serialNumber;
-     timestamp = _timestamp;
-     warrantyMilliseconds1 = _warrantyMilliseconds1;
-     warrantyMilliseconds2 = _warrantyMilliseconds2;
-     status = _status;
-     amount = _amount;
-     }
-   */
+  function Order(address _buyer, address _seller, string _name, string _serialNumber, uint256 _timestamp, uint _warrantyMilliseconds1, uint _warrantyMilliseconds2, uint _status, uint _amount) public {
+    buyer = _buyer;
+    seller = _seller;
+    name = _name;
+    serialNumber = _serialNumber;
+    timestamp = _timestamp;
+    warrantyMilliseconds1 = _warrantyMilliseconds1;
+    warrantyMilliseconds2 = _warrantyMilliseconds2;
+    status = _status;
+    amount = _amount;
+  }
 
   function getBuyer() public view returns (address) {
     return buyer;
--- core/migrations/2_deploy_contracts.js
+++ core/migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
-var Orders = artifacts.require("./Order.sol");
+var Order = artifacts.require("./Order.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Orders);
+  deployer.deploy(Order, 
+    '0x627306090abaB3A6e1400e9345bC60c78a8BEf57', 
+    '0xf17f52151EbEF6C7334FAD080c5704D77216b732', 
+    'Name',
+    "Serial Number",
+    0, 0, 0, 0, 0
+  );
 };
