e1e0400a2a02b9e115aef58f4588506963e0980a
Kim Nicole Montano <kim.nicole.montano@gmail.com>
2018-03-18 11:20:40 +0800

    Included new features

--- android/app/src/main/java/com/whitecloak/drapp/dashboard/buyer/BuyerValidateItemActivity.kt
+++ android/app/src/main/java/com/whitecloak/drapp/dashboard/buyer/BuyerValidateItemActivity.kt
@@ -1,15 +1,65 @@
 package com.whitecloak.drapp.dashboard.buyer
 
 import android.os.Bundle
+import android.support.v4.view.ViewPager
+import android.support.v4.view.ViewPager.OnPageChangeListener
 import android.support.v7.app.AppCompatActivity
 import com.whitecloak.drapp.R
+import kotlinx.android.synthetic.main.buyer_validate_item_layout.*
 
 /**
  * Created by kimnicolemontano on 2018/03/18.
  */
+const val ratio_scale = 2
+
 class BuyerValidateItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.buyer_validate_item_layout)
+        product_image_view_pager.adapter =
+                ItemImagesPagerAdapter(supportFragmentManager,
+                        arrayOf("dji_spark.png", "dji_spark_side.png", "dji_spark_bottom.png"))
+        product_image_view_pager.clipToPadding = false
+        product_image_view_pager.pageMargin = 24
+        product_image_view_pager.setPadding(48,8,48, 8)
+        product_image_view_pager.offscreenPageLimit = 3
+        product_image_view_pager.addOnPageChangeListener(object: OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+                if (state == ViewPager.SCROLL_STATE_IDLE){
+                    val currentItemPosition = product_image_view_pager.currentItem
+                    var item = getItemDisplayFragment(currentItemPosition)
+                    item.scaleImage(1F)
+                    if(currentItemPosition > 0){
+                        item = getItemDisplayFragment(currentItemPosition - 1)
+                        item.scaleImage((1 - ratio_scale).toFloat())
+                    }
+
+                    if(currentItemPosition + 1 < (product_image_view_pager.adapter as ItemImagesPagerAdapter).count){
+                        item = getItemDisplayFragment(currentItemPosition + 1)
+                        item.scaleImage((1 - ratio_scale).toFloat())
+                    }
+                }
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                var item = getItemDisplayFragment(position)
+                val ratioScale = 2
+                var scale = 1 - (positionOffset * ratioScale)
+                item.scaleImage(scale)
+
+                if(position + 1 < (product_image_view_pager.adapter as ItemImagesPagerAdapter).count){
+                    item = getItemDisplayFragment(position + 1)
+                    scale = positionOffset * ratioScale + (1 - ratioScale)
+                    item.scaleImage(scale)
+                }
+            }
+
+            override fun onPageSelected(position: Int) {
+
+            }
+        })
     }
+
+    fun  getItemDisplayFragment(position : Int) : ItemImageDisplayFragment =
+            ((product_image_view_pager.adapter as ItemImagesPagerAdapter).getItem(position) as ItemImageDisplayFragment)
 }
--- android/app/src/main/java/com/whitecloak/drapp/dashboard/buyer/ItemImageDisplayFragment.kt
+++ android/app/src/main/java/com/whitecloak/drapp/dashboard/buyer/ItemImageDisplayFragment.kt
@@ -1,4 +1,4 @@
-package com.whitecloak.drapp.camera
+package com.whitecloak.drapp.dashboard.buyer
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -7,34 +7,35 @@ import android.view.View
 import android.view.ViewGroup
 import com.squareup.picasso.Picasso
 import com.whitecloak.drapp.R
+import com.whitecloak.drapp.camera.PRODUCT_IMAGE_PATH_KEY
 import com.whitecloak.drapp.utils.notNull
 import kotlinx.android.synthetic.main.product_image_layout.view.*
 
 /**
- * Created by kimnicolemontano on 2018/03/17.
+ * Created by kimnicolemontano on 2018/03/18.
  */
-const val PRODUCT_IMAGE_PATH_KEY = "PRODUCT_IMAGE_PATH"
+class ItemImageDisplayFragment : Fragment(){
 
-class ProductImageOverlayFragment : Fragment() {
+    public fun scaleImage(scale : Float){
+        view.notNull {
+            it.scaleY = scale
+            it.invalidate()
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.product_image_layout, container, false)
+        val view = inflater.inflate(R.layout.item_image_layout, container, false)
         arguments.notNull {
-            view.product_image.alpha = 0.5F
             Picasso.get().load(it.getString(PRODUCT_IMAGE_PATH_KEY)).into(view.product_image)
         }
         return view
     }
-
     companion object {
-
-        fun newInstance(productImagePath: String): ProductImageOverlayFragment {
-
+        fun newInstance(imagePath: String) : ItemImageDisplayFragment{
             val args = Bundle()
             val asset_base_url = "file:///android_asset/"
-            args.putString(PRODUCT_IMAGE_PATH_KEY, asset_base_url + productImagePath)
-
-            val fragment = ProductImageOverlayFragment()
+            args.putString(PRODUCT_IMAGE_PATH_KEY, asset_base_url + imagePath)
+            val fragment = ItemImageDisplayFragment()
             fragment.arguments = args
             return fragment
         }
